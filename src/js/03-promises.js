77b19fb0669a3be0bc5e06b4599eae2707d0fd47
AndreyNik2 <dark.tm2011@gmail.com>
2022-10-05 20:38:33 +0300

    10th

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,10 +9,10 @@ function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
-        resolve([position, delay])
+        resolve({ position, delay })
         console.log(delay);
-  } else {
-        reject([position, delay])
+      } else {
+        reject({position, delay})
         console.log(delay);
   }
 
@@ -23,46 +23,25 @@ function createPromise(position, delay) {
 
 formEl.addEventListener('submit', handleSubmit);
 
-// function handleSubmit(event) {
-//   event.preventDefault();
-//   const { delay, step, amount } = event.currentTarget;
-//   let position = 0;
-//   let promiseDelay = delay.value - step.value;
-//   console.log(promiseDelay);
-//   for (let index = 0; index < amount.value; index++) {
-//     position += 1;
-//     promiseDelay += Number(step.value);
-//     console.log(promiseDelay);
-//     createPromise(position, promiseDelay)
-//       .then((position, promiseDelay) => {
-//         Notify.success(`✅ Fulfilled promise ${position} in ${promiseDelay}ms`);
-//         console.log(promiseDelay);
-//       })
-//       .catch((position, promiseDelay) => {
-//         Notify.failure(`❌ Rejected promise ${position} in ${promiseDelay}ms`);
-//         console.log(promiseDelay);
-//       })
-//   }
-// }
 
 function handleSubmit(event) {
   event.preventDefault();
   const { delay, step, amount } = event.currentTarget;
-  let elDelay = Number(delay.value) - Number(step.value);
+  let elDelay = Number(delay.value);
   const elStep = Number(step.value);
   const elAmount = Number(amount.value);
   for (let position = 1; position <= elAmount; position+=1) {
-    elDelay += elStep;
     console.log(elDelay);
     createPromise(position, elDelay)
-      .then(([position, elDelay]) => {
+      .then(({ position, elDelay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${elDelay}ms`);
         console.log(elDelay);
       })
-      .catch(([position, elDelay]) => {
+      .catch(({ position, elDelay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${elDelay}ms`);
         console.log(elDelay);
       })
+    elDelay += elStep;
   }
 }
 
